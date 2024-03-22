bf8a4601dfdb83a6250e99ffc8aa0369f95304b9
Nguyen Tuu <tuunguyen99@gmail.com>
2024-03-22 16:13:16 +0700

    update-windows

--- packages/shards-tech/src/index.ts
+++ packages/shards-tech/src/index.ts
@@ -5,8 +5,9 @@ export { MiraiCore as ShardsTechCore } from './core/mirai-core';
 export { IConnection } from './connection/connection';
 export * from './errors';
 
-(window as any).ShardsTech = {
-  Connection: require('./connection/mirai-connection').MiraiConnection,
-  Core: require('./core/mirai-core').MiraiCore,
-};
-
+if (typeof window === 'undefined') {
+	(window as any).ShardsTech = {
+		Connection: require('./connection/mirai-connection').MiraiConnection,
+		Core: require('./core/mirai-core').MiraiCore,
+	};
+}
